2e83fc9837434ec75036f6feacc9a684b01feba0
blsmage02 <blsmage02@outlook.kr>
2019-08-06 18:39:59 +0900

    new update

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -9,29 +9,26 @@ public class Calendar {
 		final int[] GetMaxDaysOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
 		Scanner scanner = new Scanner(System.in);
+		String PROMPT = "cal> ";
+
+		int number;
 		
-		System.out.print("반복횟수를 입력하세요 : ");
-		int repeat = scanner.nextInt();
-	
-		
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("--------------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9 10 11 12 13  14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		
-		
-		for(int i = 0; i < repeat; i++)
+		while(true)
 		{
-			System.out.print("달을 입력하세요 : ");
-			int number = scanner.nextInt();
+			System.out.println("달을 입력하세요.");
+			System.out.print(PROMPT);
+			number = scanner.nextInt();
+			
+			if(number == -1) break;
+			
+			if(number > 12) continue;
 			
 			int result = GetMaxDaysOfMonth[number - 1];
 			
 			System.out.println(number + "월은 " + result + "일 까지 있습니다.");
 		}
 		
+		System.out.println("bye~");
 		scanner.close();
 
 
