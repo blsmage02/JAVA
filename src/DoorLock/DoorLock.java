a061ef143fd57c3f75d103abeec93075a89a7ebc
blsma <blsma@DESKTOP-QK1BDAS>
2019-12-03 02:03:53 +0900

    Update project

--- src/DoorLock/DoorLock.java
+++ src/DoorLock/DoorLock.java
@@ -4,24 +4,13 @@ public class DoorLock {
 
 	static int[] myPassword = new int[8];			//내가 가질 비빌번호 배열
 	static int[] inputPassword = new int[8];		//입력한 값 임시로 갖는 배열
-
+	static String Pass= "" ;
+	
 	public static void main(String[] args) {
 
 		Frame frame = new Frame();
 
-		boolean run = true;
-
-		while(run) {
-			int count;
-
-			for(count = 0; count < myPassword.length; count++) {}
-
-			if(count < 3 && count > 9) { 
-				System.out.println("비밀번호는 4이상 8이하의 숫자를 입력해주세요.");
-				run = false;
-			}
-
-		}	
+		
 
 	}
 
--- src/DoorLock/Frame.java
+++ src/DoorLock/Frame.java
@@ -9,79 +9,97 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Frame {
-	
+
 	DoorLock doorLock = new DoorLock();
-	
+
 	public Frame() {
-		final String[] btn_Title = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#" };
+		final String[] btn_Title = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+		final String[] btn_Title2 = { "*", "#"};
 		JFrame frame = new JFrame();
 		JLabel label = new JLabel("WELCOME");
 		JPanel btnPanel = new JPanel();
-		JButton[] btns = new JButton[12];
+		JPanel TextPanel = new JPanel();
+		JButton[] btns = new JButton[10];
+		JButton[] btnsT = new JButton[2];
 
+		JPasswordField passwordtext = new JPasswordField(10);
+		JTextField text2 = new JTextField(10);
 
 		frame.setLayout(new BorderLayout());
-
+		passwordtext.setEchoChar('*');
 
 		frame.add(label, BorderLayout.NORTH);
+		frame.add(TextPanel, BorderLayout.SOUTH);
 		frame.add(btnPanel, BorderLayout.CENTER);
 
-		for(int i = 0; i < btns.length; i++) {
-			btnPanel.add(btns[i] = new JButton(btn_Title[i]));
+
+		for(int i = 0; i < 12; i++) {
+			if(i == 9) btnPanel.add(btnsT[0] = new JButton(btn_Title2[0]));
+			else if(i == 11) btnPanel.add(btnsT[1] = new JButton(btn_Title2[1]));
+			else {
+				if(i == 10) btnPanel.add(btns[i-1] = new JButton(btn_Title[i-1]));
+				else btnPanel.add(btns[i] = new JButton(btn_Title[i]));
+			}
 		}
 
-		for(int i = 0; i < btns.length; i++) {
 
-			if(btns[i].equals('*')) {		//*입력시 비밀번호 비교
-				btns[i].addActionListener(new ActionListener() {
+		btnsT[0].addActionListener(new ActionListener() {				// *버튼 클릭시 
 
-					@Override
-					public void actionPerformed(ActionEvent e) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				label.setText(ComparedKeypad());						//패널의 값 변경	
+			}
+		});
 
-						ComparedKeypad();
-						
-					}
+		btnsT[1].addActionListener(new ActionListener() { 				// #버튼 클릭시
 
-				});
-			} else if(btns[i].equals('#')) {	//#입력시 비밀번호 설정 if 값이 있을시 기존번호 입력 후 
-				btns[i].addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
 
-					@Override
-					public void actionPerformed(ActionEvent e) {
-
-
-					}
-
-				});
 			}
 
-			btns[i].addActionListener(new ActionListener() {
+		});
 
-				@Override
+
+		for(int i = 0; i < btns.length; i++) { 
+			btns[i].addActionListener(new ActionListener() {			// 숫자 클릭시
+
+				@Override 
 				public void actionPerformed(ActionEvent e) {
-					
-					
+
 				}
 
-			});
+			}); 
 		}
-		
-		btnPanel.setLayout(new GridLayout(5, 1));
+
+
+
+		TextPanel.add(passwordtext);
+		TextPanel.add(text2);
+		btnPanel.setLayout(new GridLayout(4, 2));
+		TextPanel.setLayout(new GridLayout(2, 1));
 		label.setHorizontalAlignment(JLabel.CENTER);
 		frame.setTitle("도어락");
 		frame.setVisible(true);
 		frame.setSize(440, 600/12*9);
-		frame.setResizable(false);						//사이즈 고정
+		frame.setResizable(true);						//사이즈 고정false
 		frame.setLocationRelativeTo(null);				//실행시 Frame 위치 센터
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//X버튼 클릭시 프로그램 영구 종료
 	}
-	
+
 	//비밀번호 
 	public String ComparedKeypad() {
 
 		int count = 0;
+
+		if((doorLock.myPassword[0] + doorLock.myPassword[1] + doorLock.myPassword[2]) == 0) {
+			return String.format("비밀번호를 설정해주세요.");
+		}
 
 		for(int i = 0; i < doorLock.myPassword.length; i++) {
 			if(doorLock.myPassword[i] == doorLock.inputPassword[i]) {
@@ -96,9 +114,11 @@ public class Frame {
 		return String.format("잘못된 번호가 입력되었습니다.");		
 
 	}
-	
+
 	public void CipheredKeypad() {
 
 	}
+
+
 
 }
